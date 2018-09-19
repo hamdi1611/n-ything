186f6caa45eb3948db61a860f7f4a05990881b04
hamdi1611 <hamdiahmadzuhri@gmail.com>
2018-09-19 16:02:50 +0700

    hill-climbing done

--- test.py
+++ test.py
@@ -1,29 +1,7 @@
-from Bidak import Bidak
+from hill import hill
 import help
 
-a = Bidak('Q', 1, 2)
-b = Bidak('K', 2, 0)
-c = Bidak('K', 2, 1)
-d = Bidak('r', 2, 2)
-e = Bidak('R', 3, 2)
-
-list = []
-list.append(a)
-list.append(b)
-list.append(c)
-list.append(d)
-#list.append(e)
-
-list1 = help.getListRandomized()
-list2 = help.getListRandomized()
-
-help.printResult(list1)
-help.printResult(list2)
-
-print (help.totalConflict(list1, "HITAM"))
-print (help.totalConflict(list1, "PUTIH"))
-
-print (help.totalConflict(list2, "HITAM"))
-print (help.totalConflict(list2, "PUTIH"))
-
-print (help.isBetter(list1, list2))
+answer = hill(help.getListRandomized(), 20)
+help.printResult(answer)
+print (help.totalConflict(answer, "PUTIH"))
+print (help.totalConflict(answer, "HITAM"))
