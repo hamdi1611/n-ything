c03abf9e2774559bf3092f9a1444998580ecbf44
hafizmau <hafizmaulana198@gmail.com>
2018-09-22 12:19:36 +0700

    main.py in progress

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #main program
+
+from hill import hill
+import help
+
+# menampilkan algoritma yg akan digunakan
+print ("Daftar algoritma\n")
+print ("1 : Hill Climbing\n")
+print ("2 : Simulated Annealing\n")
+print ("3 : Genetic Algoritthm\n")
+
+# menerima masukan algoritma yg akan digunakan 
+nama = input("Algoritma yang akan digunakan : ")
+
+if nama ==1:
+	soal = help.getListRandomized()
+	help.printResult(soal)
+	print (help.totalConflictSesama(soal))
+	print (help.totalConflictLawan(soal))
+	print('\n')
+	answer = hill(soal, 1000)
+	help.printResult(answer)
+	print (help.totalConflictSesama(answer))
+	print (help.totalConflictLawan(answer))
+elif nama == 2:
+	
+
+elif nama == 3:
+	
