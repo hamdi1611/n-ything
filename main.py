a90a79912e0bf4224676d1d13ab4da0dcbff0e46
hamdi1611 <hamdiahmadzuhri@gmail.com>
2018-09-22 13:34:30 +0700

    main.py good

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
-#main program
-<<<<<<< HEAD
-
 from hill import hill
-import help
-
-# menampilkan algoritma yg akan digunakan
-print ("Daftar algoritma\n")
-print ("1 : Hill Climbing\n")
-print ("2 : Simulated Annealing\n")
-print ("3 : Genetic Algoritthm\n")
-
-# menerima masukan algoritma yg akan digunakan 
-nama = input("Algoritma yang akan digunakan : ")
-
-if nama ==1:
-	soal = help.getListRandomized()
-	help.printResult(soal)
-	print (help.totalConflictSesama(soal))
-	print (help.totalConflictLawan(soal))
-	print('\n')
-	answer = hill(soal, 1000)
-	help.printResult(answer)
-	print (help.totalConflictSesama(answer))
-	print (help.totalConflictLawan(answer))
-elif nama == 2:
-	
-
-elif nama == 3:
-	
-=======
+from s_annealing import annealing
 from genetic import genetic
 import help
 
-genetic(5)
->>>>>>> da6e5b96ce0723d2cbc3c7010333847e972760ee
+# menampilkan algoritma yg akan digunakan
+print ("Daftar algoritma:")
+print ("1 : Hill Climbing")
+print ("2 : Simulated Annealing")
+print ("3 : Genetic Algoritthm")
+
+# menerima masukan algoritma yg akan digunakan 
+nama = int(input("Algoritma yang akan digunakan : "))
+
+print("\nSOAL:")
+soal = help.getListRandomized()
+help.printResult(soal)
+print (help.totalConflictSesama(soal), help.totalConflictLawan(soal))
+
+print("\nJAWABAN:")
+
+answer = []
+if nama == 1:
+	answer = hill(soal, 1000)
+elif nama == 2:
+	answer = annealing(soal, 2000)
+elif nama == 3:
+	answer = soal
+
+help.printResult(answer)
+print (help.totalConflictSesama(answer), help.totalConflictLawan(answer))
