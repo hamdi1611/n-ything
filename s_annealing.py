0c468bba0e263d1ab61cc415c585beb254e09a4f
Jose <joshos1945@yahoo.com>
2018-09-26 17:30:10 +0700

    Tambah iterasi n

--- s_annealing.py
+++ s_annealing.py
@@ -3,25 +3,26 @@ import help, math, random
 
 # Algoritma Simulated-Annealing
 # Masukan : list_of_object Bidak dan bilangan maksimum proses Simulated-Annealing
-def annealing(list_of_object, temperature):
+def annealing(list_of_object, n, temperature):
 	current_list = list(list_of_object)
 	best_list = list(list_of_object)
 	current_e = help.score(current_list)
 	best_e = current_e
-	while(temperature > 1):
+	while(n > 1):
 		next_list = list(help.getRandomTetangga(list_of_object))
 		next_e = help.score(next_list)
 		if(next_e >= current_e): 
 			if(next_e > best_e):
-				best_list = next_list
+				best_list = list(next_list)
 				best_e = next_e
-			current_list = next_list
+			current_list = list(next_list)
 			current_e = next_e
 		else:
 			if(boltzman(current_e, next_e, temperature) >= random.random()):
-				current_list = next_list
+				current_list = list(next_list)
 				current_e = next_e
 		temperature = temperature_schedule(temperature)
+		n -= 1
 	if(current_e > best_e):
 		return current_list
 	else:
@@ -31,4 +32,4 @@ def boltzman(e, ei, T):
 	return math.exp((ei - e) / T)
 
 def temperature_schedule(T):
-	return T*0.8
+	return T * 0.999
