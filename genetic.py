5ff91a5ba6178cc4b3a02bac00cfaef60558d96f
Ilham Wahabi <wahabiputra@gmail.com>
2018-10-01 06:13:06 +0700

    Add more genetic func

--- genetic.py
+++ genetic.py
@@ -1,5 +1,7 @@
 import help
-import math
+import random
+
+### Fungsi pembantu ###
 
 def convertToChromosome(states):
 	chromosome = []
@@ -7,6 +9,11 @@ def convertToChromosome(states):
 		chromosome.append(states[i].getX())
 		chromosome.append(states[i].getY())
 	return chromosome
+
+def findBestChromosome(states):
+	return states[0]
+
+### Fungsi genetic ###
 
 def init(k):
 	states = []
@@ -20,23 +27,51 @@ def evaluation(states):
 		score.append(help.score(states[i]))
 	return score
 
-def selection():
+def selection(states, population, fitness_score):
+	# Mendapatkan index chromosome dengan fitness score terendah dan tertinggi
+	worst_chromosome_index = fitness_score.index(min(fitness_score))
+	best_chromosome_index = fitness_score.index(max(fitness_score))
+
+	# Menghapus chromosome terburuk dan menggandakan chromosome terbaik
+	states.pop(worst_chromosome_index)
+	population.pop(worst_chromosome_index)
+
+	if (len(population) % 2 == 1):
+		states.append(best_chromosome_index)
+		population.append(best_chromosome_index)
+
+	# Melakukakan pengacakan pada population
+	random.shuffle(states)
+	random.shuffle(population)
+
+def crossover(states):
 	pass
 
-def crossover():
+def mutation(states):
 	pass
 
-def mutation():
-	pass
+### Fungsi utama ###
 
 def genetic(k):
-	# Mendapatkan kumpulan list of bidak sebanyak k buah	
+	# Mendapatkan kumpulan list of bidak sebanyak k buah
 	states = init(k)
 
-	# Mengubah tiap states (list of bidak)  menjadi population (list of chromosome) 
+	# Mengubah tiap states (list of bidak) menjadi population (list of chromosome) 
 	population = []
 	for i in range(len(states)):
 		population.append(convertToChromosome(states[i]))
 
 	# Mendapatkan fitness score tiap chromosome
 	fitness_score = evaluation(states)
+
+	# Melakukan seleksi pada masing-masing chromosome berdasarkan fitness score
+	selection(states, population, fitness_score)
+
+	# Menentukan kumpulan gen yang akan dilakukan crossover
+	crossover(states)
+
+	# Melakukan mutasi pada chromosome terpilih
+	mutation(states)
+
+	# Mengembalikan chromosome terbaik dari hasil mutasi
+	return findBestChromosome(states)
